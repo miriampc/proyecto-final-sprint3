097981e44551fbb998fad1cf77c0a3e366a6d3ae
Miriam Peralta <miiriampc@gmail.com>
2017-05-02 17:55:29 -0500

    validando inputs

--- assets/js/app.js
+++ assets/js/app.js
@@ -44,7 +44,5 @@ inputMsj.forEach(function(e,i){
     input[i].setAttribute("title",e.title);
     input[i].classList.add("verify");
   });
-  // input[i].addEventListener("focus",function(){
-  //   input[i].classList.remove("verify");
-  // });
+  
 });
